148a3368fe0d69e9b8481b7e8d665a2c1f220b9a
klein-mask <klein.asuka0505@gmail.com>
2021-03-18 00:42:59 +0900

    ""不要なファイルとコメントの削除

--- docker/postgres/init/create-table.sql
+++ docker/postgres/init/create-table.sql
@@ -7,15 +7,3 @@ create table if not exists users (
     updated_at timestamp,
     deleted_at timestamp
 );
-
-/* 
-create table if not exists moneys (
-    id bigint primary key,
-    balance bigint default 0,
-    user_id bigint not null unique,
-    history_id varchar(20),
-    created_at timestamp,
-    updated_at timestamp,
-    deleted_at timestamp
-);
-*/
