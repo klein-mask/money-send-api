8bc79c725b0e753b85a99ef77cb495d90e9e7e56
klein-mask <klein.asuka0505@gmail.com>
2021-03-19 17:19:07 +0900

    パスワードの暗号化と複合化

--- docker/postgres/init/create-table.sql
+++ docker/postgres/init/create-table.sql
@@ -1,7 +1,7 @@
 create table if not exists users (
     id bigint primary key,
     name varchar(20) not null unique,
-    password varchar(20) not null,
+    password varchar(100) not null,
     balance bigint default 0,
     is_balance_receivable boolean default false,
     created_at timestamp,
